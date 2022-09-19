f01e0014f82a692003b693381f804ce88e24fce2
Daniel Ward <dward1502@gmail.com>
2022-09-19 11:21:43 -0700

    worked on dynamic pages

--- pages/projects/[id].jsx
+++ pages/projects/[id].jsx
@@ -1,14 +1,102 @@
-import React from "react";
-import Navigation from "../../components/Navigation/pageNav";
+import React from 'react';
+import Navigation from '../../components/Navigation/pageNav';
+import Image from 'next/image';
 
-import { projects } from "../../data/projects";
+import { projects } from '../../data/projects';
+import styles from './projects.module.scss';
 
 const SpecificProjectPage = ({ projectInfo }) => {
   return (
     <>
       <Navigation />
-      <h1>Specific Project</h1>
-      <p>{projectInfo.title}</p>
+      <div className={styles.content}>
+        <h1 className={styles.title}>{projectInfo.title}</h1>
+        <section className={styles.info}>
+          <div className={styles.description}>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+              magnam obcaecati labore delectus accusantium consequatur velit
+              incidunt laudantium, repellendus maiores porro mollitia nihil
+              soluta voluptates aperiam optio error tempore vel nemo quos?
+              Sapiente eos voluptas pariatur molestias. Aliquam vero explicabo
+              perferendis delectus velit beatae maiores dolores nam. Libero,
+              quidem recusandae.
+            </p>
+            <ul>
+              <li>Information of project in bullet points</li>
+              <li>Information of project in bullet points</li>
+              <li>Information of project in bullet points</li>
+            </ul>
+          </div>
+          <div className={styles.tech}>
+            <h3>Technologies Used</h3>
+            <div className={styles.techList}>
+              <div className={styles.techBox}>HTML</div>
+            </div>
+            <div className={styles.links}>
+              <a href='#'>Live link</a>
+              <a href='#'>Github</a>
+            </div>
+          </div>
+        </section>
+        <section className={styles.deviceImages}>
+          <div className={styles.desktopImage}>
+            <div className={styles.imageBoxDesk}>
+              <Image
+                src={'/images/psarHomepage.png'}
+                alt='Picture of a breaking wave.'
+                layout='fill'
+                objectFit='cover'
+                objectPosition='center'
+              />
+            </div>
+          </div>
+          <div className={styles.mobileImage}>
+            <div className={styles.imageBoxMobile}>
+              <Image
+                className={styles.img}
+                src={'/images/psar_phone.png'}
+                alt='Picture of a breaking wave.'
+                layout='fill'
+                objectFit='cover'
+                objectPosition='center'
+              />
+            </div>
+          </div>
+        </section>
+        <section className={styles.pages}>
+          <div className={styles.pagesBox}>
+            <Image
+              className={styles.img}
+              src={'/images/psar_home_full.png'}
+              alt='Picture of a breaking wave.'
+              layout='fill'
+              objectFit='cover'
+              objectPosition='center'
+            />
+          </div>
+          <div className={styles.pagesBox}>
+            <Image
+              className={styles.img}
+              src={'/images/psar_join_full.png'}
+              alt='Picture of a breaking wave.'
+              layout='fill'
+              objectFit='cover'
+              objectPosition='center'
+            />
+          </div>
+          <div className={styles.pagesBox}>
+            <Image
+              className={styles.img}
+              src={'/images/psar_crmls_full.png'}
+              alt='Picture of a breaking wave.'
+              layout='fill'
+              objectFit='cover'
+              objectPosition='center'
+            />
+          </div>
+        </section>
+      </div>
     </>
   );
 };
@@ -29,25 +117,5 @@ export const getStaticPaths = async () => {
   }));
   return { paths, fallback: false };
 };
-
-// export async function getStaticProps(context) {
-//   const projectID = context.params.slug;
-//   console.log(projectID);
-
-//   console.log(`This is json in static props ${JSON.stringify(projects)}`);
-
-//   return {
-//     props: {
-//       projectInfo: projects,
-//     },
-//   };
-// }
-
-// export function getStaticPaths() {
-//   return {
-//     paths: [{ params: { id: "1" } }],
-//     fallback: false,
-//   };
-// }
 
 export default SpecificProjectPage;
