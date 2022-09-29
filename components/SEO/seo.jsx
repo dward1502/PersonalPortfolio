87c56e1840143478229c31f67a24f04ea872e188
Daniel Ward <dward1502@gmail.com>
2022-09-29 16:28:32 -0700

    added default SEO

---
+++ components/SEO/seo.jsx
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+
+const DOMAIN = "https://www.danjward.com";
+const DEFAULT_OG_IMAGE = '/images/breakingWave.jpg';
+
+export default function Seo({
+    title="Daniel Wards Portfolio",
+    description="Daniel Ward is a Fullstack developer with an emphasis on Front End dev work. Provides UX/UI and web application fundamentals to your business needs.",
+    siteName="Daniel Ward",
+    canonical=DOMAIN,
+    ogImage=DEFAULT_OG_IMAGE,
+    ogType="website",
+    twitterHandle = "@anonymous_dbow"
+}){
+    return (
+        <Head>
+            <title key="title">{`${title} - ${siteName}`}</title>
+            <meta name="description" content={description} />
+            <meta key="og_type" property='og:type' content={ogType} />
+            <meta key="og_title" property='og:title' content={title}/>
+            <meta key="og_description" property='og:description' content={description} />
+            <meta key="og_locale" property="og:locale" content="en_US" />
+            <meta key="og_site_name" property="og:site_name" content={siteName} />
+            <meta key="og_url" property='og:url' content={canonical ?? DOMAIN} />
+            <meta key="og_site_name" property="og:site_name" content={siteName} />
+            <meta key="og_image" property='og:image' content={ogImage ?? DEFAULT_OG_IMAGE} />
+            <meta key="og_image:alt" property="og:image:alt" content={`${title} | ${siteName}`} />
+            <meta key="og_image:width" property='og:image:width' content="1200" />
+            <meta key="og_image:height" property="og:image:height" content='630' />
+            <meta name='robots' content='index,follow' />
+            <meta key="twitter:card" name='twitter:card' content='summary_large_image' />
+            <meta key="twitter:site" name="twitter:site" content={twitterHandle} />
+            <meta key="twitter:creator" name="twitter:creator" content={twitterHandle} />
+            <meta key="twitter:title" property="twitter:title" content={title} />
+            <meta key="twitter:description" property='twitter:description' content={description} />
+            <link rel='canonical' href={canonical ?? DOMAIN} />
+            <link rel='shortcut icon' href='/dwLogoWhite.ico' />
+        </Head>
+    )
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,26 @@
 import Layout from '../components/layout';
+import Seo from '../components/SEO/seo';
 
 import '../sass/globals.scss';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, canonical }) {
   return (
-    <Layout>
-      <Component {...pageProps}/>
-    </Layout>
-  )
+    <>
+      <Seo canonical={canonical} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
+MyApp.getInitialProps = ({ctx}) => {
+  const isProd = process.env.NODE_ENV === "production";
+  const base = isProd ? "https://www.danjward.com" : "http://localhost:3000";
+  const {asPath} = ctx;
+  const canonical = base + asPath;
+  return {
+    canonical
+  }
+}
 export default MyApp
